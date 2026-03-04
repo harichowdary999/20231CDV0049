a5dd742f2635297b87f361b3afc250b232bd509e
Hari <darapanenihari29@gmail.com>
2026-03-04 11:30:59 +0530

    intial-commit

---
+++ AddTwoNumbers.java
@@ -0,0 +1,12 @@
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        int num1 = 5; // The first number
+        int num2 = 10; // The second number
+        int sum; // Variable to store the result
+
+        sum = num1 + num2; // Adding the two numbers using the '+' operator
+
+        // Printing the result to the console
+        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
